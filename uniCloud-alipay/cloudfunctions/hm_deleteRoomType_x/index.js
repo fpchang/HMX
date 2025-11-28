a028d1a72de5c9d6277a5f28f5c06c388580cf3f
fpchang <hanfengcfp@outlook.com>
2025-11-28 16:09:39 +0800

    new conte

---
+++ uniCloud-alipay/cloudfunctions/hm_deleteRoomType_x/index.js
@@ -0,0 +1,10 @@
+'use strict';
+exports.main = async (event, context) => {
+	//event为客户端上传的参数
+	let {_id} =event;
+	console.log("hm_deleteRoomType",event);
+		const db =uniCloud.database();
+		const dCmd=db.command;
+	const result = await db.collection('hm-roomType').doc(_id).remove();
+	return result;
+};

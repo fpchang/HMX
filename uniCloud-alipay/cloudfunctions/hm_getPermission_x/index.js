684d57fb37aef1d78491ead908436b1a672f4b29
fpchang <hanfengcfp@outlook.com>
2025-12-03 20:46:59 +0800

    3

--- uniCloud-alipay/cloudfunctions/hm_getPermission_x/index.js
+++ uniCloud-alipay/cloudfunctions/hm_getPermission_x/index.js
@@ -20,7 +20,6 @@ exports.main = async (event, context) => {
 	 	const roleObj = await dbJQL.collection("hm-employee").where({hotel_id,phone}).get();
 		let role_name = "normal";
 		if(roleObj.data.length>0){	
-			console.warn("3333333333",roleObj.data.length)
 			role_name = roleObj.data[0]['role'];			
 						
 		}else{
@@ -30,17 +29,13 @@ exports.main = async (event, context) => {
 		}
 		if(role_name == "administrator"){
 			const resper = await dbJQL.collection("hm-permission").field("permission_name").get();
-			console.log("aaaaa>>",resper);
 			let perArr = resper.data.map(item=>item.permission_name);
 			resper.data =perArr;
-			console.log("bbbb>>",perArr);
 			return resper;
 		}
-		console.warn("role_name",role_name);
 	 	const permission_select  = dbJQL.collection("hm-permission").getTemp();
 	 	const role_permission_select = dbJQL.collection("hm-role-permission").where( {role_name}).getTemp();
 	 	const res = await dbJQL.collection(role_permission_select,permission_select).get(); 
-		console.warn("222222>>",res)
 	 	res.data=  formatPermissionToArray(res.data);
 		return res;
 	 }catch(e){

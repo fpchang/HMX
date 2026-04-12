3a11e48c2a9f1502d684f6f01315c317c061abb4
fpchang <hanfengcfp@outlook.com>
2026-04-12 13:35:35 +0800

    1

--- uniCloud-alipay/cloudfunctions/hm_addEmployee/index.js
+++ uniCloud-alipay/cloudfunctions/hm_addEmployee/index.js
@@ -14,6 +14,23 @@ exports.main = async (event, context) => {
 	// const result = await db.collection('hm-hotel').doc(_id).update({
 	// 	employee: dCmd.push([employeeObj])
 	// });
+	const {phone,account,account_id} =employeeObj;
+	if(!phone&&!account&&!account_id){
+		throw new Error("无有效账号信息");
+	}
+	const sql = phone?`phone=='${phone+""}'`:`account=='${account+""}'`;
+	const targetRes = await dbJQL.collection('hm-user').where(sql).get();
+	const user = targetRes.data[0];
+	if(!user){
+		throw new Error("没有匹配的账号")
+		return {
+			code:404,
+			message:"添加的员工没有注册账号"
+		}
+	}
+	const u_id = user._id;
+	employeeObj.account_id=u_id;
+	//console.log('1111',employeeObj)
 	const result = await dbJQL.collection('hm-employee').add(employeeObj);
 	return result;
 };

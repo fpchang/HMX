8c2a66471963bf265452613f954efd623510cb0d
fpchang <hanfengcfp@outlook.com>
2026-04-25 23:28:59 +0800

    1

--- uniCloud-alipay/cloudfunctions/hm_login/index.js
+++ uniCloud-alipay/cloudfunctions/hm_login/index.js
@@ -251,7 +251,7 @@ function getUser(phone, token) {
 		"nickName": "",
 		"phone": phone,
 		//"account":null,
-		"email": "",
+		//"email": "",
 		"password": "",
 		"userId": phone,
 		"userName": `用户${phone.substr(-4)}`,
